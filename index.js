fb634688a6155690c3dac192732dd2b2496ff934
Baryczka25 <gusedubary21@gmail.com>
2025-11-23 02:12:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const ClientSFTP = require('ssh2-sftp-client');
 
-const TOKEN = "1442010203921973340";
+const TOKEN = "MTQ0MjAxMDIwMzkyMTk3MzM0MA.G9DX4K.JRe-TxIONj-om_fIRTcuYvjFt1UHgBm5M2MNVI";
 const SFTP_HOST = "enx-cirion-95.enx.host";
 const SFTP_PORT = 2022;
 const SFTP_USER = "dwho4u2a.f75839cd";
